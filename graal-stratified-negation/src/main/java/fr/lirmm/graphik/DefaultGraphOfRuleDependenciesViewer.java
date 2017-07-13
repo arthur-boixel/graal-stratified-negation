3589f30d991b6d01fd73dfe52e691d38341c8e49
arthur <arthur.boixel@protonmail.ch>
2017-07-13 14:50:22 +0200

    Linking and reviewing the stylesheet

--- graal-stratified-negation/src/main/java/fr/lirmm/graphik/DefaultGraphOfRuleDependenciesViewer.java
+++ graal-stratified-negation/src/main/java/fr/lirmm/graphik/DefaultGraphOfRuleDependenciesViewer.java
@@ -1,6 +1,5 @@
 package fr.lirmm.graphik;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -11,7 +10,6 @@ import org.graphstream.graph.implementations.SingleGraph;
 
 import fr.lirmm.graphik.graal.api.core.GraphOfRuleDependencies;
 import fr.lirmm.graphik.graal.api.core.Rule;
-import fr.lirmm.graphik.graal.core.grd.DefaultGraphOfRuleDependencies;
 import fr.lirmm.graphik.util.graph.scc.StronglyConnectedComponentsGraph;
 
 public class DefaultGraphOfRuleDependenciesViewer {
@@ -26,53 +24,16 @@ public class DefaultGraphOfRuleDependenciesViewer {
 		return instance;
 	}
 	
-	
+	/***********/
+	/*** GRD ***/
+	/***********/
 	public Graph getGraph(GraphOfRuleDependencies g)
 	{
 		Graph graphDisp = new SingleGraph("Graph Of Rules Dependencies");
-		computeVertex(g , graphDisp);
+		computeVertices(g , graphDisp);
 		addDependencies(g , graphDisp);
 		
-		graphDisp.addAttribute("ui.stylesheet", "node {\n" + 
-				"	shape: circle;\n" + 
-				"	fill-color: green;\n" + 
-				"	text-color: black;\n" + 
-				"	text-mode : normal;\n" + 
-				"	text-size : 15px;\n" + 
-				"	stroke-mode: plain;\n" + 
-				"	stroke-color: green;\n" + 
-				"	stroke-width: 2px;\n" + 
-				"	size-mode : fit;\n" + 
-				"	text-background-mode : rounded-box;\n" + 
-				"	text-background-color : green;\n" +
-				"}\n" + 
-				"\n" + 
-				"node.bad {\n" + 
-				"	shape: circle;\n" + 
-				"	fill-color: red;\n" + 
-				"	text-color: black;\n" + 
-				"	text-mode : normal;\n" + 
-				"	text-size : 15px;\n" + 
-				"	stroke-mode: plain;\n" + 
-				"	stroke-color: red;\n" + 
-				"	stroke-width: 2px;\n" + 
-				"	size-mode : fit;\n" + 
-				"	text-background-mode : rounded-box;\n" +
-				"	text-background-color : red;\n" +
-				"}\n" + 
-				"\n" + 
-				"edge {\n" +
-				"   fill-color : black;\n" +
-				"   text-color : black;\n" +
-				"}\n" +
-				"\n" +
- 				"edge.bad {\n" + 
-				"	fill-color : red;\n" +  
-				"}\n" +
-				"\n" +
-				"edge.moins {\n" +
-				"   fill-color : orange;\n" +
-				"}\n");
+		graphDisp.addAttribute("ui.stylesheet", "url(file:styles.css)");
 		
 		graphDisp.addAttribute("ui.quality");
 		graphDisp.addAttribute("ui.antialias");
@@ -80,150 +41,8 @@ public class DefaultGraphOfRuleDependenciesViewer {
 		return graphDisp;
 	}
 	
-	public Graph getSCCGraph(GraphOfRuleDependencies graph) {
-		StronglyConnectedComponentsGraph<Rule> sccGraph = graph.getStronglyConnectedComponentsGraph();
-		Graph graphDisp = new SingleGraph("Graph of Strongly Connected Components");
-		
-		
-		/* Vertices */
-		
-		for(int i = 0 ; i < sccGraph.getNbrComponents() ; i++)
-		{
-			Node n = graphDisp.addNode("C" + i);
-			n.addAttribute("label", "C"+i);
-			n.addAttribute("c", sccGraph.getComponent(i).toString());
-		}
-		
-		
-		/* Edges */
-		for(int i = 0 ; i < sccGraph.getNbrComponents() ; i++)
-		{
-			for(Rule src : sccGraph.getComponent(i))
-			{
-				for(int j = 0 ; j < sccGraph.getNbrComponents() ; j++)
-				{
-					Set<Rule> s = sccGraph.getComponent(j);
-					String edgeID = "C" + i + "|C" + j;
-					
-					for(Rule dest : ((DefaultLabeledGraphOfRuleDependencies) graph).getInhibitedRules(src))
-					{
-						if(s.contains(dest) && graphDisp.getEdge(edgeID) == null && i != j)
-						{
-							Edge e = graphDisp.addEdge(edgeID.toString(),
-									"C" + i,
-									"C" + j,
-									true
-									);
-							e.setAttribute("label", '-');
-							e.addAttribute("ui.class", "moins");
-						}
-					}
-					
-					for(Rule dest : graph.getTriggeredRules(src))
-					{
-						if(s.contains(dest) && graphDisp.getEdge(edgeID) == null && i != j)
-						{
-							Edge e = graphDisp.addEdge(edgeID.toString(),
-									"C" + i,
-									"C" + j,
-									true
-									);
-							e.setAttribute("label", '+');
-							e.addAttribute("ui.class", "plus");
-						}
-					}
-				}
-			}
-		}
-		
-		/* Colors on vertices */
-		List<List<Rule>> l = ((DefaultLabeledGraphOfRuleDependencies)graph).getBadCircuits();
-		
-		if(l != null)
-		{
-			boolean touch;
-			
-			for(int i = 0 ; i < sccGraph.getNbrComponents() ; i++)
-			{
-				touch = false;
-				for(List<Rule> c : l)
-				{
-					for(Rule r : c)
-					{
-						if(sccGraph.getComponent(i).contains(r))
-						{
-							graphDisp.getNode("C" + i).addAttribute("ui.class", "bad");
-							touch = true;
-							break;
-						}
-					}
-					
-					if(touch)
-						break;
-				}
-			}
-		}
-		
-		
-		
-		/* Style */
-		graphDisp.addAttribute("ui.stylesheet", "node {\n" + 
-				"	shape: circle;\n" + 
-				"	fill-color: green;\n" + 
-				"	text-color: black;\n" + 
-				"	text-mode : normal;\n" + 
-				"	text-size : 15px;\n" + 
-				"	stroke-mode: plain;\n" + 
-				"	stroke-color: green;\n" + 
-				"	stroke-width: 2px;\n" + 
-				"	size-mode : fit;\n" + 
-				"	text-background-mode : rounded-box;\n" +
-				"	text-background-color : green;\n" +
-				"}\n" + 
-				"\n" + 
-				"node.bad {\n" + 
-				"	shape: circle;\n" + 
-				"	fill-color: red;\n" + 
-				"	text-color: black;\n" + 
-				"	text-mode : normal;\n" + 
-				"	text-size : 15px;\n" + 
-				"	stroke-mode: plain;\n" + 
-				"	stroke-color: red;\n" + 
-				"	stroke-width: 2px;\n" + 
-				"	size-mode : fit;\n" + 
-				"	text-background-mode : rounded-box;\n" +
-				"	text-background-color : red;\n" + 
-				"}\n" + 
-				"\n" + 
-				"edge {\n" + 
-				"	fill-color: black;\n" +
-				"   text-color: black;\n" +
-				"}\n" + 
-				"\n" + 
-				"edge.moins {\n" + 
-				"	fill-color : orange;\n" +  
-				"}");
-		
-		
-		
-		return graphDisp;
-	}
 	
-	public void display(GraphOfRuleDependencies g)
-	{
-		Graph graphDisp = new SingleGraph("Graph Of Rules Dependencies");
-		computeVertex(g , graphDisp);
-		addDependencies(g , graphDisp);
-
-	    System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-	    
-	    graphDisp.addAttribute("ui.stylesheet", "url('styles.css')");
-		graphDisp.addAttribute("ui.quality");
-		graphDisp.addAttribute("ui.antialias");
-		graphDisp.display();			
-	}
-	
-	private void computeVertex(GraphOfRuleDependencies g , Graph graphDisp)
+	private void computeVertices(GraphOfRuleDependencies g , Graph graphDisp)
 	{
 		for(Rule r : g.getRules())
 		{
@@ -247,6 +66,7 @@ public class DefaultGraphOfRuleDependenciesViewer {
 			}
 		}
 	}
+	
 	
 	private void addDependencies(GraphOfRuleDependencies g , Graph graphDisp)
 	{
@@ -304,4 +124,124 @@ public class DefaultGraphOfRuleDependenciesViewer {
 			}
 		}
 	}
+	/***********/
+	/*** SCC ***/
+	/***********/
+	public Graph getSCCGraph(GraphOfRuleDependencies graph) {
+		StronglyConnectedComponentsGraph<Rule> sccGraph = graph.getStronglyConnectedComponentsGraph();
+		Graph graphDisp = new SingleGraph("Graph of Strongly Connected Components");
+		
+		computeSccVertices(sccGraph , graphDisp , graph);
+		addSccDependencies(sccGraph, graphDisp, graph);
+		
+		
+		/* Style */
+		graphDisp.addAttribute("ui.stylesheet", "url(file:styles.css)");
+		
+		return graphDisp;
+	}
+		
+	private void computeSccVertices(StronglyConnectedComponentsGraph<Rule> sccGraph , Graph graphDisp , GraphOfRuleDependencies graph)
+	{
+		/* Vertices */
+		
+		for(int i = 0 ; i < sccGraph.getNbrComponents() ; i++)
+		{
+			Node n = graphDisp.addNode("C" + i);
+			n.addAttribute("label", "C"+i);
+			n.addAttribute("c", sccGraph.getComponent(i).toString());
+		}
+		
+		/* Colors on vertices */
+		List<List<Rule>> l = ((DefaultLabeledGraphOfRuleDependencies)graph).getBadCircuits();
+		
+		if(l != null)
+		{
+			boolean touch;
+			
+			for(int i = 0 ; i < sccGraph.getNbrComponents() ; i++)
+			{
+				touch = false;
+				for(List<Rule> c : l)
+				{
+					for(Rule r : c)
+					{
+						if(sccGraph.getComponent(i).contains(r))
+						{
+							graphDisp.getNode("C" + i).addAttribute("ui.class", "bad");
+							touch = true;
+							break;
+						}
+					}
+					
+					if(touch)
+						break;
+				}
+			}
+		}
+	}
+	
+	private void addSccDependencies(StronglyConnectedComponentsGraph<Rule> sccGraph , Graph graphDisp , GraphOfRuleDependencies graph)
+	{
+		/* Edges */
+		for(int i = 0 ; i < sccGraph.getNbrComponents() ; i++)
+		{
+			for(Rule src : sccGraph.getComponent(i))
+			{
+				for(int j = 0 ; j < sccGraph.getNbrComponents() ; j++)
+				{
+					Set<Rule> s = sccGraph.getComponent(j);
+					String edgeID = "C" + i + "|C" + j;
+					
+					for(Rule dest : ((DefaultLabeledGraphOfRuleDependencies) graph).getInhibitedRules(src))
+					{
+						if(s.contains(dest) && graphDisp.getEdge(edgeID) == null && i != j)
+						{
+							Edge e = graphDisp.addEdge(edgeID.toString(),
+									"C" + i,
+									"C" + j,
+									true
+									);
+							e.setAttribute("label", '-');
+							e.addAttribute("ui.class", "moins");
+						}
+					}
+					
+					for(Rule dest : graph.getTriggeredRules(src))
+					{
+						if(s.contains(dest) && graphDisp.getEdge(edgeID) == null && i != j)
+						{
+							Edge e = graphDisp.addEdge(edgeID.toString(),
+									"C" + i,
+									"C" + j,
+									true
+									);
+							e.setAttribute("label", '+');
+							e.addAttribute("ui.class", "plus");
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	
+	/********************/
+	/*** Fast Display ***/
+	/********************/
+	public void display(GraphOfRuleDependencies g)
+	{
+		Graph graphDisp = new SingleGraph("Graph Of Rules Dependencies");
+		computeVertices(g , graphDisp);
+		addDependencies(g , graphDisp);
+
+	    System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+	    
+	    graphDisp.addAttribute("ui.stylesheet", "url(file:styles.css)");
+		graphDisp.addAttribute("ui.quality");
+		graphDisp.addAttribute("ui.antialias");
+		graphDisp.display();			
+	}
+	
+	
 }
